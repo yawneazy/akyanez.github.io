6ee6b84563eefc5a070971d40feee5a4df726198
Alyssa Karina <yaneza@email.arizona.edu>
2023-01-05 11:23:17 -0700

    seeing if changes work - main computer

--- Desktop/my-website/my-app/src/App.js
+++ Desktop/my-website/my-app/src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Updating
+          Updating...dfgr
+          
         </a>
       </header>
     </div>

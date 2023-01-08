07a1fe915beedd6ee65de37db227a124afcd5ccb
Alyssa Karina <yaneza@email.arizona.edu>
2023-01-07 20:41:54 -0700

    added main image

--- Desktop/my-website/my-app/src/App.js
+++ Desktop/my-website/my-app/src/App.js
@@ -1,21 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-// import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About'
 // import NavbarList from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <>
     <Navbar/>
-      {/* <Routes> */}
-      {/* <Route exact path='/' element={<Homepage/>}/>
-      <Route path='/home' element={<Homepage/>}/>
+      <Routes>
+       <Route exact path='/' element={<Home/>}/>
+       <Route path='/About' element={<About/>}/>
+      {/* <Route path='/home' element={<Homepage/>}/>
       <Route path='/Introduction' element={<Homepage/>}/>
-      <Route path='/about' element={<About/>}/>
+      <Route path='/about' element={<About/>}/> */}
       {/* <Route path='/:title' element={<Details/>}/> */}
-      {/* </Routes> */}
+      </Routes>
       {/* <Footer/> */}
      </> 
     
--- Desktop/my-website/my-app/src/Components/Navbar/Navbar.js
+++ Desktop/my-website/my-app/src/Components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import ReactDOM from 'react-dom';   
 import "./Navbar.css";
 // import NavLogo from "./nav-images/NavLogo.png";
+// import homeImage from "./images/office.jpg";
 
 function Navbar(props) {
     //useState Hook
@@ -28,20 +29,21 @@ function Navbar(props) {
         
         <div className="header">
           
-               {/* <img src = {NavLogo} className="logo"></img> */}
-            {/* <div className ="h1"> Digital Black Religion</div> */}
+            {/* <img src = {NavLogo} className="office-image"></img> */}
+            <div className="name">Alyssa K. Yanez</div>
             <div className="navbar">
               <ul>
                   <li><button value={navBarItems[0]} onClick={e => handleClick(e)}>{navBarItems[0]}</button></li>
                   <li><button value={navBarItems[1]} onClick={e => handleClick(e)}>{navBarItems[1]}</button></li>
                   <li><button value={navBarItems[2]} onClick={e => handleClick(e)}>{navBarItems[2]}</button></li>
                   <li><button value={navBarItems[3]} onClick={e => handleClick(e)}>{navBarItems[3]}</button></li>
+                  <li><button value={navBarItems[4]} onClick={e => handleClick(e)}>{navBarItems[4]}</button></li>
               </ul>
             </div>
         </div>
     )
 }
-    const navBarItems = ['About','Data','Survey','Subscribe']; 
+    const navBarItems = ['About','Work','Contact','Resume','Play']; //Play
 
 
 export default Navbar;
